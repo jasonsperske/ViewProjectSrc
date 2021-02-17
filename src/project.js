37dbff64cddef48143d3ceaa7f22eedee6eddedf
Jason Sperske <jason@sperske.com>
2021-02-16 21:03:09 -0800

    Cleaned up code
    Links are now more consistently generated

--- src/project.js
+++ src/project.js
@@ -1,11 +1,33 @@
+function isURL(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
+
+function link(label, urlFragment, base = '') {
+  let url = isURL(urlFragment) ? urlFragment : base + urlFragment;
+  const html = [];
+  html.push(`<div><code>${label}:</code>`);
+  if (isURL(url)) {
+    html.push(`<a href="${url}" target="_blank">${urlFragment}</a>`);
+  } else {
+    html.push(label);
+  }
+  html.push(`</div>`);
+  return html.join('');
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     chrome.tabs.sendMessage(tabs[0].id, {from: 'popup', subject: 'ProjectInfo'}, (info) => {
       const projectName = info['project-src-name'] ?? chrome.i18n.getMessage("ViewProjectDefaultTitle");
       const base = info['project-src-base'] ?? '';
-      let html = [];
+      const html = [];
       html.push(`<h1>${projectName}</h1>`);
-      html.push(`<div><code>SRC:</code><a href="${info['project-src']}" target="_blank">${info['project-src']}</a></div>`)
+      html.push(link('SRC', info['project-src']));
       if (info['project-src-component']) {
         let components = info['project-src-component'];
         if (!(components instanceof Array)) {
@@ -15,7 +37,7 @@ window.addEventListener('DOMContentLoaded', () => {
         components.forEach((component) => {
           const componentType = Object.keys(component)[0];
           const componentLabel = component[componentType];
-          html.push(`<div><code>${componentType}:</code> <a href="${base}${componentLabel}" target="_blank">${componentLabel}</a></div>`);
+          html.push(link(componentType, componentLabel, base));
         });
         html.push(`</div>`);
       }
